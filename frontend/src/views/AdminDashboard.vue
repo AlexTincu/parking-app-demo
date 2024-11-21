1c787dd2286fbb225ed7ea72568e1d594a687679
AlexTincu <tz.alexx@gmail.com>
2024-11-21 12:22:25 +0200

    remove search button in admindashboard component

--- frontend/src/views/AdminDashboard.vue
+++ frontend/src/views/AdminDashboard.vue
@@ -14,13 +14,6 @@
         placeholder="Search Location by Name or Address"
         class="w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
       />
-      <button
-        class="bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-md"
-        @click="searchLocations()"
-        @keyup.enter="searchLocations"
-      >
-        Search
-      </button>
     </div>
 
     <!-- Locations Section -->
@@ -167,10 +160,8 @@ const paginatedLocations = computed(() => {
 })
 
 const totalPages = computed(() => store.getters['admin/locations'].totalPages)
-
-const searchLocations = () => {
-  fetchLocations()
-}
+const revenue = computed(() => store.getters['admin/revenue'])
+const error = computed(() => store.getters['admin/error'])
 
 const updateRate = (locationId, newRate) => {
   store.dispatch('admin/updateHourlyRate', { locationId, newRate })
